ddf4c85267da7df53bc06225e514ec4bda2c4c8d
amoulis <adrien.moulis@gmail.com>
2015-08-09 16:03:07 +0200

    skeleton for events

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # a small python script for testing text-based RPG
 
 import character
+import random
+import event
 
 # INIT
 player = character.Character()
 ennemy = character.Character(5, 1, 5, 0)
+random.seed(); # random event
 
-player.getDamages(ennemy.dealtDamages())
+"""while player._life > 0:
+	rand_event = random.randint(1,3)
+	if rand_event == 1:
+		event.eventNothing()
+	elif rand_event == 2:
+		event.eventBattle()
+	else:
+		event.eventSpaceStation()
+	player.getDamages(ennemy.dealtDamages())
+"""
 print(player._shield)
