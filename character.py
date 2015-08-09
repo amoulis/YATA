4a781683ff8a5e239a0ee1a3f7a4a507ece9a538
amoulis <adrien.moulis@gmail.com>
2015-08-09 13:32:17 +0200

    created character class

---
+++ character.py
@@ -0,0 +1,18 @@
+class Character:
+
+	def __init__(self, life=5, strength=5, armor=5, xp=0):
+		self._life = life
+		self._strength = strength
+		self._armor = armor
+		self._xp = xp
+
+	def getDamages(self, damages)
+
+		if(armor >= damages)
+			armor = armor - 5
+
+	def isAlive(self):
+		if life == 0
+			return 1
+		else
+			return 0
