6c9f524d445c78e6a252fb3f218e39de9f008352
amoulis <adrien.moulis@gmail.com>
2015-08-09 15:59:29 +0200

    created skeleton for file 'event'

---
+++ event.py
@@ -0,0 +1,10 @@
+
+def eventNothing():
+	print("The planet you found is empty, nothing to be done")
+	print("Jump to next planet ? Y - N")
+
+def eventBattle():
+	print("Woaw ! An ennemy ship is here. Get ready for battle !")
+
+def eventSpaceStation():
+	print("Welcome to a friendly space station. You can do some business here")
