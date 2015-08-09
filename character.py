e4af8e491c3438e2260b2aa55aa045967e1bd70c
amoulis <adrien.moulis@gmail.com>
2015-08-09 13:50:38 +0200

    first methods in character class

--- character.py
+++ character.py
@@ -6,12 +6,17 @@ class Character:
 		self._armor = armor
 		self._xp = xp
 
-	"""def getDamages(self, damages)
+	def getDamages(self, damages):
+		self._armor = self._armor - damages
+		# if damages exceeded armor, dealt damages to life
+		if self._armor != 0 & self._armor < 0:
+			self._life = self.life + self._armor; # because armor < 0 no need to use '-'
+			self._armor = 0
+			print("[WARN] Armor broken !")
 
-		if armor >= damages:
-			armor = armor - 5
-		else if armor < damages && 
-		"""
+	def dealtDamages(self):
+		return self._strength
+
 	def isAlive(self):
 		if life == 0:
 			return 1
