de6d03b6f3d13ebf38a9a5901a60cff184e8b4c6
amoulis <adrien.moulis@gmail.com>
2015-08-09 16:39:34 +0200

    energy and other options added

--- main.py
+++ main.py
@@ -6,22 +6,29 @@ import event
 
 # INIT
 player = character.Character()
-ennemy = character.Character(5, 1, 5, 0)
+ennemy = character.Character(5, 1, 5, 0, 0)
 random.seed(); # random event
 
 order = "go"
 while player._life > 0 and order!="quit":
-	order = input("What do you want to do ? [move - repare - save - quit]")
+	order = input("What do you want to do ? [move - repare - save - quit]\n >>> ")
 
 	if order == "move":
-		rand_event = random.randint(1,3)
-		if rand_event == 1:
-			event.eventNothing()
-		elif rand_event == 2:
-			event.eventBattle()
+		if player.isEnergyEmpty():
+			print("You have no more energy. You will die alone drifting in space")
 		else:
-			event.eventSpaceStation()
+			player._energy = player._energy-1
+			rand_event = random.randint(1,3)
+			if rand_event == 1:
+				event.eventNothing()
+			elif rand_event == 2:
+				event.eventBattle()
+			else:
+				event.eventSpaceStation()
 	elif order == "repare":
 		print("reparing...")
 	elif order == "save":
 		print("saving...")
+	else:
+		if order != "quit":
+			print("Sorry not a valid option. Try again please")
