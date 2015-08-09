08b3095d599a4fd37548922d5eacd8f8602bb73c
amoulis <adrien.moulis@gmail.com>
2015-08-09 16:26:35 +0200

    skeleton of game mechanic

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ player = character.Character()
 ennemy = character.Character(5, 1, 5, 0)
 random.seed(); # random event
 
-"""while player._life > 0:
-	rand_event = random.randint(1,3)
-	if rand_event == 1:
-		event.eventNothing()
-	elif rand_event == 2:
-		event.eventBattle()
-	else:
-		event.eventSpaceStation()
-	player.getDamages(ennemy.dealtDamages())
-"""
-print(player._shield)
+order = "go"
+while player._life > 0 and order!="quit":
+	order = input("What do you want to do ? [move - repare - save - quit]")
+
+	if order == "move":
+		rand_event = random.randint(1,3)
+		if rand_event == 1:
+			event.eventNothing()
+		elif rand_event == 2:
+			event.eventBattle()
+		else:
+			event.eventSpaceStation()
+	elif order == "repare":
+		print("reparing...")
+	elif order == "save":
+		print("saving...")
