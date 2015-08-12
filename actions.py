8d303597180d1d28e3c8d89118f3eb0f9ac5e728
amoulis <adrien.moulis@gmail.com>
2015-08-12 09:37:54 +0200

    added actions file

---
+++ actions.py
@@ -0,0 +1,16 @@
+def actionRepare(player):
+	if player._credits - 50 >= 0:
+		player._life = player._lifeMax;
+		player._credits = player._credits - 50
+		log.information("Repare done !")
+	else:
+		log.warning("Sorry, you don't ahve enough credits")
+
+def actionRecharge(player):
+	if player._credits - 10 >= 0:
+		player._energy = player._energy + 1
+		player._credits = player._credits - 10
+		if player._energy > player._energyMax:
+			player._energy = player._energyMax
+	else:
+		log.warning("Sorry, you don't have enough credits")
