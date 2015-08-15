f4c445e721fe1376de06bb25bd38b1ba771714a4
amoulis <adrien.moulis@gmail.com>
2015-08-15 11:35:34 +0200

    rules in game

--- menu.py
+++ menu.py
@@ -1,11 +1,12 @@
 import character
 import save
 import sys
+import utils
 
 def menu(player):
 	choice = ""
 	while choice != "load" or choice != "play" or choice != "quit":
-		choice = input("What do you want to do ?\n play \n load \n quit \n >>> ")
+		choice = input("What do you want to do ?\n play \n load \n rules \n quit \n >>> ")
 		player = character.Character()
 		if choice == "load":
 			save.load(player)
@@ -13,3 +14,5 @@ def menu(player):
 			return;
 		elif choice == "quit":
 			sys.exit("good bye")
+		elif choice == "rules":
+			utils.printFile("text/rules")
